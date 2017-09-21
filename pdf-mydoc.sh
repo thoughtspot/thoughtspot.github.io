d9adfc4e95b2a63b6a43c66766be93ec56863b33
mary.anthony <mary.anthony@thoughtspot.com>
2017-09-21 09:31:07 -0700

    Updating configs
    Signed-off-by: mary.anthony <mary.anthony@thoughtspot.com>

--- pdf-mydoc.sh
+++ pdf-mydoc.sh
@@ -4,11 +4,17 @@ echo 'Killing all Jekyll instances'
 kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 
+echo 'Deleting all old pdfs'
+rm -rf pdf/mydoc.pdf;
+echo "done";
+
 echo "Building PDF-friendly HTML site for Mydoc ...";
 bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_mydoc_pdf.yml;
 echo "done";
 
 echo "Building the PDF ...";
 prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/mydoc.pdf;
-echo "done";
 
+echo "Done. Look in the pdf directory to see if it printed successfully."
+
+open pdf/mydoc.pdf;
