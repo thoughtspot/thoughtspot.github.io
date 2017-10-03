72cf400a2f59939cc8671c7c562e8278cf7b9be7
mary.anthony <mary.anthony@thoughtspot.com>
2017-10-03 14:57:37 -0700

    Updating master
    Signed-off-by: mary.anthony <mary.anthony@thoughtspot.com>

---
+++ build-site.sh
@@ -0,0 +1,16 @@
+# /bsh/sh
+
+for v in 4.4 4.1
+  do
+    # Checkout a versioned branch with the version name
+    git checkout $v
+    # Create a configuration file that sets a new baseurl based d on version
+    echo "baseurl : /$v" > _config.$v.yml
+    echo "exclude : " >> _config.$v.yml
+    echo "  - $v"  >> _config.$v.yml
+    # Build using both the basic configuration file and the version config file
+    bundle exec jekyll build --config _config.yml,_config.$v.yml -d /tmp/$v/
+    rm _config.$v.yml
+  done
+
+git checkout master
