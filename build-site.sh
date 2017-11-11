67239adabead18837092617a785a24d274169787
mary.anthony <mary.anthony@thoughtspot.com>
2017-11-10 16:44:30 -0800

    Update script
    Signed-off-by: mary.anthony <mary.anthony@thoughtspot.com>

--- build-site.sh
+++ build-site.sh
@@ -1,15 +1,26 @@
 # /bsh/sh
 
+if [ -z "$1" ]
+  then
+    echo "The command format is: bash-build.sh BRANCH_NAME VERSION_VALUE"
+    exit
+  else
+    if [ -z "$2" ]
+    then
+        echo "Use the format: bash-build.sh BRANCH_NAME VERSION_VALUE"
+        exit
+    fi
 
+    # Checkout a versioned branch with the version name
+    git checkout $1
+    # Create a configuration file that sets a new baseurl based d on version
+    echo "baseurl : /$2" > _config.$2.yml
+    echo "exclude : " >> _config.$2.yml
+    echo "  - $2"  >> _config.$2.yml
+    # Build using both the basic configuration file and the version config file
+    bundle exec jekyll build --config _config.yml,_config.$2.yml -d /tmp/$2/
+    rm _config.$2.yml
 
-# Checkout a versioned branch with the version name
-git checkout $1
-# Create a configuration file that sets a new baseurl based d on version
-echo "baseurl : /$2" > _config.$2.yml
-echo "exclude : " >> _config.$2.yml
-echo "  - $2"  >> _config.$2.yml
-# Build using both the basic configuration file and the version config file
-bundle exec jekyll build --config _config.yml,_config.$2.yml -d /tmp/$2/
-rm _config.$2.yml
+    git checkout master
 
-git checkout master
+fi
