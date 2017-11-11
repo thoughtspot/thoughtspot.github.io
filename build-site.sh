d1ad50021d144a5ca8a19234ac6bfc175716c9fb
mary.anthony <mary.anthony@thoughtspot.com>
2017-11-10 16:29:13 -0800

    fixing script
    Signed-off-by: mary.anthony <mary.anthony@thoughtspot.com>

--- build-site.sh
+++ build-site.sh
@@ -9,7 +9,7 @@ echo "baseurl : /$2" > _config.$2.yml
 echo "exclude : " >> _config.$2.yml
 echo "  - $2"  >> _config.$2.yml
 # Build using both the basic configuration file and the version config file
-bundle exec jekyll build --config _config.yml,_config.$2.yml -d /tmp/$1/
+bundle exec jekyll build --config _config.yml,_config.$2.yml -d /tmp/$2/
 rm _config.$2.yml
 
 git checkout master
