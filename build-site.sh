8e805ff46f83f9309d1c4eb20407db124684f91d
mary.anthony <mary.anthony@thoughtspot.com>
2018-04-26 11:02:22 -0700

    Adding production var to bundle call
    Signed-off-by: mary.anthony <mary.anthony@thoughtspot.com>

--- build-site.sh
+++ build-site.sh
@@ -71,7 +71,7 @@ echo "  - $2"  >> _config.$2.yml
 echo "branch_url : /$1" >> _config.$2.yml
 
 # Build using both the basic configuration file and the version config file
-bundle exec jekyll build --config _config.yml,_config.$2.yml -d /tmp/$2/
+JEKYLL_ENV=production bundle exec jekyll build --config _config.yml,_config.$2.yml -d /tmp/$2/
 rm _config.$2.yml
 
 if git checkout master; then
